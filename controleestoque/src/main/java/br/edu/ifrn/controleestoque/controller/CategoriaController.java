516010bf22e0d49c36e28728f509d52c50c9dbc4
Andriéria Dantas <andrieria.ad@gmail.com>
2023-09-20 10:24:10 -0300

    Ainda não tá ok

--- controleestoque/src/main/java/br/edu/ifrn/controleestoque/controller/CategoriaController.java
+++ controleestoque/src/main/java/br/edu/ifrn/controleestoque/controller/CategoriaController.java
@@ -4,28 +4,36 @@ package br.edu.ifrn.controleestoque.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.edu.ifrn.controleestoque.domain.categoria.Categoria;
 import br.edu.ifrn.controleestoque.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping("categorias")
 public class CategoriaController {
 
     @Autowired
-    CategoriaRepository categoriaRepository;
+    private CategoriaRepository categoriaRepository;
 
     @PostMapping
-    public void salvar(@RequestBody Categoria categoria){
-        categoriaRepository.save(categoria);
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid Categoria categoria, UriComponentsBuilder uriBuilder){
+        Categoria categoriaLocal = categoriaRepository.save(categoria);
+        var uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoriaLocal.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     } 
 
     @GetMapping("/{id}")
@@ -35,9 +43,24 @@ public class CategoriaController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<Categoria>> listar(Pageable paginacao){
+    public ResponseEntity<Page<Categoria>> listar(@PageableDefault(size=4, sort = {"nome"}) Pageable paginacao){
          var categorias = categoriaRepository.findAll(paginacao);
          return ResponseEntity.ok(categorias);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id) {
+        var categoria = categoriaRepository.getReferenceById(id);
+        categoriaRepository.delete(categoria);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity atualizar (@RequestBody Categoria categoria) {
+        var categoriaLocal = categoriaRepository.getReferenceById(categoria.getId());
+        categoriaRepository.save(categoriaLocal);
+        return ResponseEntity.ok(categoriaLocal);
     }
     
 }
--- controleestoque/src/main/java/br/edu/ifrn/controleestoque/domain/categoria/Categoria.java
+++ controleestoque/src/main/java/br/edu/ifrn/controleestoque/domain/categoria/Categoria.java
@@ -5,20 +5,27 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+/*import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern; */
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name="categoria")
 @Table(name="categoria")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @EqualsAndHashCode(of="id")
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Nome;
+    @NotBlank
+    /*@Pattern(ege)*/
+    private String nome;
 }
